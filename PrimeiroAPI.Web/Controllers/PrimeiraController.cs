ca48a401104b5026d2ff3a7b6044863f6c775d97
Leandro Tiberio <leandrostiberio@gmail.com>
2022-05-06 23:49:11 -0300

    Criando Primeira API

---
+++ PrimeiroAPI.Web/Controllers/PrimeiraController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+namespace PrimeiroAPI.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class PrimeiraController : ControllerBase
+    {
+        [HttpGet]
+
+        public string PrimeiraAPI()
+        {
+            return "Teste";
+        }
+        [HttpGet("segundoApi")]
+        public string SegundaAPI()
+        {
+            return "TesteDois";
+        }   
+        [HttpGet("terceiraApi")]
+        public IActionResult OutraAPI(int entrada)
+        {
+            if(entrada ==1)
+            {
+                return BadRequest("1 Não é uma entrada valída");
+            }
+            return Ok(entrada);
+        } 
+    }
+}
+
+
+
+
+
+
